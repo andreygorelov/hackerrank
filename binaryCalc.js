b1ddba74738165d2dcee924926d7e7f9fdb22ce1
Andrey <agorelov@hotmail.com>
2016-03-09 20:01:49 -0500

    Added files via upload

---
+++ binaryCalc.js
@@ -0,0 +1,78 @@
+var res = document.getElementById("res");
+var btn0 = document.getElementById("btn0");
+var btn1 = document.getElementById("btn1");
+var btnClr = document.getElementById("btnClr");
+var btnEql = document.getElementById("btnEql");
+var btnSum = document.getElementById("btnSum");
+var btnSub = document.getElementById("btnSub");
+var btnDiv = document.getElementById("btnDiv");
+var btnMul = document.getElementById("btnMul");
+
+function addRes(val) {
+  var tempRes = res.innerHTML;
+  res.innerHTML = tempRes + "" + val;
+}
+btn0.onclick = function() {
+  addRes(btn0.innerHTML);
+}
+btn1.onclick = function() {
+  addRes(btn1.innerHTML);
+}
+btnSum.onclick = function() {
+  addRes(btnSum.innerHTML);
+}
+btnSub.onclick = function() {
+  addRes(btnSub.innerHTML);
+}
+btnMul.onclick = function() {
+  addRes(btnMul.innerHTML);
+}
+btnDiv.onclick = function() {
+  addRes(btnDiv.innerHTML);
+}
+
+var calc = function(x, y, fn) {
+  return Math.floor((fn(parseInt(x, 2), parseInt(y, 2))).toString(2));
+}
+var sum = function(x, y) {
+  return x + y;
+}
+var sub = function(x, y) {
+  return x - y;
+}
+var mul = function(x, y) {
+  return x * y;
+}
+var div = function(x, y) {
+  return x / y;
+}
+
+btnEql.onclick = function() {
+  var tempResArr = res.innerHTML.split('');
+  var x = '';
+  var y = '';
+  var fn = '';
+  for (var i = 0; i < tempResArr.length; i++) {
+    if (isNaN(tempResArr[i])) {
+      if (tempResArr[i] == '+') {
+        fn = sum;
+      } else if (tempResArr[i] === '*') {
+        fn = mul;
+      } else if (tempResArr[i] === '-') {
+        fn = sub;
+      } else if (tempResArr[i] === '/') {
+        fn = div;
+      }
+    } else {
+      if (fn === '') {
+        x += "" + tempResArr[i];
+      } else {
+        y += "" + tempResArr[i];
+      }
+    }
+  }
+  res.innerHTML = calc(x, y, fn);
+}
+btnClr.onclick = function() {
+  res.innerHTML = '';
+}
